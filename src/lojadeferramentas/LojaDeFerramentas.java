b51c34dcabdcc72e8f9385c091c3b26170b774ae
USUARIO <USUARIO@LABORATORIO-PC>
2019-07-22 17:36:19 -0300

    Exemplo de tela usando o DAO

--- src/lojadeferramentas/LojaDeFerramentas.java
+++ src/lojadeferramentas/LojaDeFerramentas.java
@@ -8,6 +8,7 @@ package lojadeferramentas;
 import java.util.List;
 import model.bean.Customer;
 import model.dao.CustomerDAO;
+import view.customer.showCustomers;
 
 public class LojaDeFerramentas {
 
@@ -15,25 +16,9 @@ public class LojaDeFerramentas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("vamos testar o mysql");
-
-        try {
-
-            CustomerDAO clienteDAO = new CustomerDAO();
-            List<Customer> listaDeClientes = clienteDAO.buscar();
-            
-            for(Customer cliente : listaDeClientes){
-                System.out.println("_________");
-                System.out.println(cliente.getCustomerId());
-                System.out.println(cliente.getCustomerName());
-                System.out.println(cliente.getPhoneNumber());
-                System.out.println(cliente.getAddress());
-            }
-            
-        } catch (Exception e) {
-            System.out.printf(" não carregou: " + e.getMessage());
-        } finally {
-
-        }
+        System.out.println("Iniciando o programa");
+        
+        new showCustomers().setVisible(true);
+        
     }
 }
